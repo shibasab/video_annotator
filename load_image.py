7213332ba0d09649f0bc9a08fd63ba05baec6575
shibasab <yakyu.hira89@outlook.jp>
2020-05-16 22:21:22 +0900

    動画読み込み処理

---
+++ load_image.py
@@ -0,0 +1,38 @@
+import cv2
+from PIL import Image, ImageTk
+
+
+class VideoFrame:
+    def __init__(self, vid_path):
+        self.idx = 0
+        self.imgs = self.load(vid_path)
+        self.shape = self.imgs[0].shape
+
+    def get_tkframe(self):
+        img = self.imgs[self.idx]
+        img_pil = Image.fromarray(img)
+        img_tk = ImageTk.PhotoImage(img_pil)
+        return img_tk
+
+    def count(self):
+        self.idx += 1
+
+    def load(self, vid_path):
+        imgs = []
+
+        cap = cv2.VideoCapture(vid_path)
+        if not cap.isOpened():
+            print("Could not open video.")
+            return
+
+        while True:
+            ret, frame = cap.read()
+
+            if ret:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                imgs.append(frame)
+
+            else:
+                break
+
+        return imgs
