550154558ce0ad5be8fa97740ab9bba0b1b54141
shibasab <yakyu.hira89@outlook.jp>
2020-05-19 15:10:01 +0900

    ボタン一の修正

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ class App(tk.Frame):
             "Impact": 0,
             "Follow-through": 0,
             "Finish": 0}
-        self.pack()
         self.create_widgets()
 
     def create_widgets(self):
@@ -27,41 +26,37 @@ class App(tk.Frame):
             width=self.video_frame.shape[0],
             height=self.video_frame.shape[1])
 
-        self.canvas.pack()
+        self.canvas.pack(anchor=tk.NW)
         self.show_img()
 
         self.btn = tk.Button(
             self,
             text="next",
             command=lambda: self.change_img())
-        self.btn.pack(anchor=tk.NW, side="top")
+        self.btn.grid(row=1, column=0, sticky=tk.W)
 
         self.make_classbtn()
 
         self.save_btn = tk.Button(
             self, text="save", command=lambda: save_label(
                 self.video_frame.vid_path, self.classes))
-        self.save_btn.pack(anchor=tk.NW)
+        self.save_btn.grid(row=3, column=0, sticky=tk.W)
 
     def show_img(self):
         self.img = self.video_frame.get_tkframe()
-        self.canvas.create_image(0, 0, anchor="nw", image=self.img)
+        self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
 
     def change_img(self, n=1):
         self.video_frame.count(n)
-
-        if self.video_frame.idx > len(self.video_frame.imgs):
-            self.video_frame.idx = 0
-
         self.show_img()
 
     def make_classbtn(self):
-        for k in self.classes:
+        for i, k in enumerate(self.classes):
             classbtn = tk.Button(
                 self,
                 text=k,
                 command=lambda K=k: self.set_eventnum(K, self.video_frame.idx))
-            classbtn.pack()
+            classbtn.grid(row=2, column=i, padx=5)
             self.buttons.append(classbtn)
 
     def set_eventnum(self, k, idx):
@@ -71,10 +66,10 @@ class App(tk.Frame):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry('640x320')
     root.resizable(width=10, height=10)
-
+    root.geometry("640x320")
     video_frame = VideoFrame("./video/0.mp4")
     app = App(video_frame, master=root)
+    app.pack()
 
     app.mainloop()
